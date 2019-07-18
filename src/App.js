d2a71c704078cebda949c25d42ae303986418928
wendellcalixtoo <wendellcalixtoo@gmail.com>
2019-07-17 21:26:16 -0300

    chore: Adição do Header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
+import Header from './components/Header';
 import GlobalStyle from './styles/global';
 import Routes from './routes';
 
 function App() {
     return (
         <BrowserRouter>
+            <Header />
             <Routes />
             <GlobalStyle />
         </BrowserRouter>
